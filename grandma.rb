e5a8785aec1ffc05739fee854fb23c9c5479c3d7
Lydia B <lydiabaik@gmail.com>
2019-12-12 05:45:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma
+  "HUH?! SPEAK UP, SONNY!"
+  #string == string.upcase
+  "NO, NOT SINCE 1938!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
