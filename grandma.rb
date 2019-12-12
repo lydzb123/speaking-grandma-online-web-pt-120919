f129d418e3604bf388327ca8e777f3882579482a
Lydia B <lydiabaik@gmail.com>
2019-12-12 05:50:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,6 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-
   elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase
