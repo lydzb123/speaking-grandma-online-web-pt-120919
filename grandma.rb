32272e0c971ae77e408d2d3301f4653d718ed41c
Lydia B <lydiabaik@gmail.com>
2019-12-12 05:50:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(string)
   elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase
-  return "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
 
